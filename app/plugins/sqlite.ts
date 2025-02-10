cb4e1d04eb76064c9e893e7d99f29fc27844bc16
Fritz Gäbler <fritzlmgaebler@gmail.com>
2025-02-10 15:49:41 +0100

    add sql initialization
    Sql gets initialized in the sqlite.ts file. A database object is now
    added to the fastify object

---
+++ app/plugins/sqlite.ts
@@ -0,0 +1,21 @@
+import sqlite3 from 'sqlite3'
+import fp from 'fastify-plugin'
+import { open, Database } from 'sqlite'
+import path from 'path'
+
+/**
+ * This plugins adds sqlite3 support
+ * @see https://www.npmjs.com/package/sqlite3
+ * @see https://www.npmjs.com/package/sqlite
+**/
+
+export default fp(async (fastify) => {
+  const dbPath = path.resolve(__dirname, '../../database/db.sqlite');
+  console.log('dbPath', dbPath);
+  const db: Database = await open({
+	filename: dbPath,
+	driver: sqlite3.Database
+  });
+
+  fastify.decorate('sqlite', db);
+})
