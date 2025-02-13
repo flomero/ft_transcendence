47a9b01e166514651515e21cc6e42dbcf3ac5c2f
Paul Großmann <git@paulgrossmann.de>
2025-02-13 12:54:47 +0000

    working getting of google profile infos and jwt singing

--- app/plugins/google-oauth.ts
+++ app/plugins/google-oauth.ts
@@ -1,6 +1,38 @@
 import { FastifyPluginAsync } from 'fastify';
 import fp from 'fastify-plugin'
 import oauthPlugin from '@fastify/oauth2';
+import { OAuth2Token } from '@fastify/oauth2';
+
+interface GoogleUserInfo {
+    id: string;
+    email: string;
+    verified_email: boolean;
+    name: string;
+    given_name: string;
+    family_name: string;
+    picture: string;
+}
+
+async function getGoogleProfile(accessToken: string): Promise<GoogleUserInfo> {
+    const response = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch Google profile: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data as GoogleUserInfo;
+}
+
+export interface JWTContent {
+    id: string;
+    name: string;
+    token: OAuth2Token;
+}
 
 const googleOAuthPlugin: FastifyPluginAsync = async (fastify, opts) => {
     fastify.register(oauthPlugin, {
@@ -19,9 +51,23 @@ const googleOAuthPlugin: FastifyPluginAsync = async (fastify, opts) => {
 
     fastify.get('/login/google/callback', async (request, reply) => {
         try {
-            const token = await (fastify as any).googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request);
-            console.log('Token:', token);
-            reply.send(token);
+            const token = await (fastify as any).googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request) as OAuth2Token;
+            const userInfo = await getGoogleProfile(token.token.access_token);
+            if (!userInfo.verified_email) {
+                throw new Error('Google account not verified');
+            }
+
+            const jwtContent: JWTContent = {
+                id: userInfo.id,
+                name: userInfo.name,
+                token: token
+            }
+
+            const jwtToken = await fastify.jwt.sign(jwtContent, {
+                expiresIn: token.token.expires_in,
+            });
+
+            reply.send(jwtToken);
         } catch (error) {
             fastify.log.error(error);
             reply.status(500).send(error);
--- app/routes/game/new/index.ts
+++ app/routes/game/new/index.ts
@@ -2,9 +2,10 @@ import { FastifyPluginAsync } from "fastify"
 
 const newGame: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
 	fastify.get('/', { websocket: true }, (socket, req) => {
-	  socket.on("message", (message: string) => {
-		  socket.send("pong");
+		socket.on("message", (message: string) => {
+			socket.send("pong");
+		});
 	});
-  });
 };
+
 export default newGame;
