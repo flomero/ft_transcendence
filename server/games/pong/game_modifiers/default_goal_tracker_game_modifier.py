2d777fcb5f2a1295e8127693f10db8404b1b2712
lgreau <137093922+GREAULouen@users.noreply.github.com>
2025-02-16 16:50:23 +0100

    Separated goal tracking & elimination on goal count into 2 game modifiers

---
+++ server/games/pong/game_modifiers/default_goal_tracker_game_modifier.py
@@ -0,0 +1,20 @@
+from ..pong_modifier_base import PongModifierBase
+from ..multiplayer_pong import MultiplayerPong
+from ...game_registry import GAME_REGISTRY
+
+
+class DefaultGoalTrackerGameModifier(PongModifierBase):
+    name = "default_goal_tracker_game_modifier"
+
+    def __init__(self):
+        super().__init__()
+
+    def on_goal(self, game: MultiplayerPong, player_id=-1):
+        """Handle goal counting & ball reset"""
+
+        if not player_id in range(game.player_count):
+            print(f"on_goal triggered w/ player id {player_id} when the game has {game.player_count} players")
+            return
+
+        game.player_goals[player_id] += 1
+        game.reset_ball(ball_id=0)
---
+++ server/games/pong/game_modifiers/goal_count_based_elimination_game_modifier.py
@@ -0,0 +1,25 @@
+from ..pong_modifier_base import PongModifierBase
+from ...game_registry import GAME_REGISTRY
+from ..multiplayer_pong import MultiplayerPong
+
+
+class GoalCountBasedEliminationGameModifier(PongModifierBase):
+    name = "goal_count_based_elimination_game_modifier"
+
+    def __init__(self):
+        super().__init__()
+
+        self.life_count = GAME_REGISTRY["pong"]["game_modifiers"][self.name]["life_count"]
+
+    def on_goal(self, game: MultiplayerPong, player_id=-1):
+        """On goal eliminates players that have reached life_count"""
+
+        if game.player_goals[player_id] >= self.life_count:
+            game.results[player_id] = len(
+                [
+                    goals
+                    for goals in game.player_goals
+                    if goals < self.life_count
+                ]
+            )
+            game.trigger_modifiers('on_player_elimination', player_id=player_id)
