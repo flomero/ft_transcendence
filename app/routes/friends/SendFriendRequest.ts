59b66eacc3ed298e925cb9fd0d0366b2259ff964
Fritz Gäbler <fritzlmgaebler@gmail.com>
2025-02-17 13:46:05 +0100

    change options in friend request add and accept. preHandlers need to be added onec oauth works

--- app/routes/friends/SendFriendRequest.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginAsync } from 'fastify';
-import { handleSendFriendRequest } from '../../services/friends/handleSendFriendRequest';
-import friendRequestSchema from '../../schemas/friends/friendRequestSchema';
-
-const send_friend_request: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-	  fastify.post('/send-friend-request', { schema: friendRequestSchema }, handleSendFriendRequest);
-}
-
-export default send_friend_request;
--- app/routes/friends/acceptFriendRequest.ts
+++ app/routes/friends/acceptFriendRequest.ts
@@ -1,9 +1,14 @@
-import { FastifyPluginAsync } from "fastify"
+import { FastifyPluginAsync } from "fastify";
 import handleAcceptFriendRequest from "../../services/friends/handleAcceptFriendRequest";
 import friendRequestSchema from '../../schemas/friends/friendRequestSchema';
+//import { userHook } from './testAddUser';
 
 const acceptFriendRequest: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-	  fastify.post('/accept-friend-request', { schema: friendRequestSchema }, handleAcceptFriendRequest);
+    fastify.post('/accept-friend-request', {
+        schema: friendRequestSchema,
+       // preHandler: userHook,
+        handler: handleAcceptFriendRequest
+    });
 }
 
 export default acceptFriendRequest;
