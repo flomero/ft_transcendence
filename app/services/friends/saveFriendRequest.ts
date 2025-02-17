d63aa4908d7095e4fa826be24d2432e3f63304bb
Fritz Gäbler <fritzlmgaebler@gmail.com>
2025-02-17 15:11:42 +0100

    seperate request creation and modification to "accepted" state in two functions

--- app/services/friends/saveFriendRequest.ts
+++ app/services/friends/saveFriendRequest.ts
@@ -1,8 +1,8 @@
 import { FastifyInstance } from 'fastify';
 
-export async function saveFriendRequest(friendId: string, userId: string, statusAcceppted: number, fastify: FastifyInstance): Promise<void> {
+export async function saveFriendRequest(friendId: string, userId: string, fastify: FastifyInstance): Promise<void> {
 	const sql = 'INSERT INTO users_friends (senderId, receiverId, accepted) VALUES ($1, $2, $3)';
-	const ret = await fastify.sqlite.run(sql, [userId, friendId, statusAcceppted]);
+	const ret = await fastify.sqlite.run(sql, [userId, friendId, 0]);
 	if (ret === undefined)
 		throw new Error('Failed to save friend request');
 }
