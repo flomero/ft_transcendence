b2a59c766d9dc6d9d4897b197520da253b16e265
fritz-gabler <fritzlmgaebler@gmail.com>
2025-02-18 21:29:08 +0100

    add base working logic for game wensocket

--- app/routes/game/connect-to-game-websocket/connectToGameWebsocket.ts
+++ app/routes/game/connect-to-game-websocket/connectToGameWebsocket.ts
@@ -1,11 +1,13 @@
 import { FastifyPluginAsync } from "fastify"
-import userHook from "./testPreHandler";
+import { userHook } from "./testPreHandler";
 import gameWebsocketHandler from "../../../services/game/websocket/gameWebsocketHandler";
+import { hookSchema } from "./testPreHandler";
 
 const gameWebsocket: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
 	fastify.get('/', {
 	  websocket: true,
 	  // preHandler: AuthMiddleware
+    schema: hookSchema,
 	  preHandler: userHook
 	}, gameWebsocketHandler);
   };
--- app/services/game/websocket/gameWebsocketHandler.ts
+++ app/services/game/websocket/gameWebsocketHandler.ts
@@ -4,6 +4,10 @@ import { WebSocket } from 'ws';
 
 const gameWebsocketHandler = async (connection: WebSocket, request: FastifyRequest): Promise<void> => {
 	connection.send("hallo");
+  connection.on('message', (message) => {
+    connection.send(`echo: ${message}`);
+    console.log('received: %s', message);
+  });
 }
 
 export default gameWebsocketHandler;
