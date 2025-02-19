c8d53cb36c4f3582450c2b36f0e518af729ed282
Fritz Gäbler <fritzlmgaebler@gmail.com>
2025-02-19 17:40:25 +0100

    add userId to fastify instance for typescript

---
+++ app/@types/fastify.d.ts
@@ -0,0 +1,13 @@
+import { FastifyRequest } from "fastify";
+
+declare module 'fastify' {
+    interface FastifyRequest {
+      userId: string;
+      userName: string;
+      isAuthenticated: boolean;
+    }
+
+    interface FastifyInstance {
+      userId?: string;
+    }
+  }
