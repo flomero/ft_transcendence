427c60ca957bb6008e187adc1c3853b4b734fa0e
Fritz Gäbler <fritzlmgaebler@gmail.com>
2025-02-19 17:42:59 +0100

    add test pre handler because i could not figure out how to use the google oauth

--- app/routes/game/connect-to-game-websocket/testPreHandler.ts
+++ app/routes/game/connect-to-game-websocket/testPreHandler.ts
@@ -1,7 +1,29 @@
-import fastify from "fastify";
+import { FastifyRequest, FastifyReply } from "fastify";
 
-fastify.addHook('onRequest', (request, reply, done) => {
-  // Some code
-  done()
-})
-export { userHook };
+const headersJsonSchema = {
+  type: 'object',
+  properties: {
+    'x-userId': { type: 'string' }
+  },
+  required: ['x-userId']
+}
+
+export const hookSchema = {
+  headers: headersJsonSchema
+}
+
+interface UserRequestHeaders {
+  Headers: {
+    'x-userId': string;
+  };
+}
+
+export const userHook = async (req: FastifyRequest< UserRequestHeaders >, reply: FastifyReply) => {
+  let userid = req.raw.headers["x-userid"];
+
+  if (Array.isArray(userid)) {
+        userid = userid[0];
+    }
+  console.log("THAT IS THE HEADER: " + userid);
+  req.server.userId = userid;
+};
