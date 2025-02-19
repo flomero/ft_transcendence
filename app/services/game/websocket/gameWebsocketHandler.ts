5ca49b7a8bc69fd819722bcd196f42e4b810bfe5
Fritz Gäbler <fritzlmgaebler@gmail.com>
2025-02-19 17:42:29 +0100

    add little type chants in the websocket handler

--- app/services/game/websocket/gameWebsocketHandler.ts
+++ app/services/game/websocket/gameWebsocketHandler.ts
@@ -5,8 +5,8 @@ import { WebSocket } from 'ws';
 const gameWebsocketHandler = async (connection: WebSocket, request: FastifyRequest): Promise<void> => {
 	connection.send("hallo");
   connection.on('message', (message) => {
-    connection.send(`echo: ${message}`);
-    console.log('received: %s', message);
+    connection.send(`echo: ${message} from user: ${request.server.userId}`);
+    console.log('received: %s, user: %s', message, request.server.userId);
   });
 }
 
