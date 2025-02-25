4da4959e81784fb10a172ae90c88845076d3796f
fritz-gabler <fritzlmgaebler@gmail.com>
2025-02-25 17:56:59 +0100

    Add new, better-structured features to Match (saving match in DB doesn't work).

--- app/services/game/websocket/Match.ts
+++ app/services/game/websocket/Match.ts
@@ -1,27 +1,21 @@
 import Player from './Player';
 import { Database } from 'sqlite';
-import { v4 as uuidv4 } from 'uuid';
-
-type GameOptions =
-  { gameType: 'VanillaDouble'; }
-    | { gameType: 'ModdedDouble'; modifiers: Modifiers[] }
-    | { gameType: 'VanillaMulti'; }
-    | { gameType: 'ModdedMulti'; modifiers: Modifiers[] };
-
-type Modifiers = "blackwhole" | "speedUpBall";
+import { randomUUID } from 'crypto';
 
 class Match {
-  private _players: Player[] = [];
-  private _matchId: string = uuidv4();
-  private _numberOfPlayers: number = 0;
+  private _matchId: string = randomUUID();
   private _gameOptions: GameOptions;
+  private _players: Player[] = [];
+  private _numberOfPlayers: number = 0;
   private _connectionToDB: Database;
+  private _gameStatus: "waiting" | "playing" | "finished" = "waiting";
 
 
-  constructor(firstPlayer: Player, gameOptions: GameOptions, dbConneection: Database) {
+  constructor(gameOptions: GameOptions, dbConneection: Database) {
     this._gameOptions = gameOptions;
     this._numberOfPlayers = this.setNumberOfPlayers(gameOptions);
     this._connectionToDB = dbConneection;
+    this._gameStatus = "waiting";
   }
 
   private setNumberOfPlayers(gameOptions: GameOptions): number {
@@ -32,19 +26,45 @@ class Match {
   }
 
   async addPlayerToGame(player: Player) {
-    if (this.isBeforFinalPlayer() === true) {
-      this._players.push(player);
-      //Notify all other players that a new player has joined? FLO
-    }
+    if (this.canPlayerBeAdded(player) === false)
+      throw new Error("Player can't be added to game");
+    else if (this.isBeforFinalPlayer() === true)
+      this.registerPlayer(player);
     else if (this.isFinalPlayer() === true) {
-      this._players.push(player);
-      this.addMatchToDB();
-      // start game
-      //Notify all other players that a new player has joined? FLO
+      this.registerPlayer(player);
+      await this.startGame();
     }
     else {
       player.sendMessage("Match is full");
     }
+  }
+
+  private canPlayerBeAdded(player: Player): boolean {
+    if (this._gameStatus !== "waiting")
+      return (false);
+    else if (this.isAddedAlready(player) === true)
+      return (false);
+    else if (this.isGameFull() == true)
+      return (false);
+    else if (player.currentState !== "WaitingForMessage" )
+      return (false);
+    return (true);
+  }
+
+  private isAddedAlready(player: Player): boolean {
+    if (this._players.includes(player) === true)
+      return (true);
+    return (false);
+  }
+
+  private sendMatchIdToPlayer(player: Player) {
+    player.sendMessage("matchId: " + this.matchId);
+  }
+
+  private registerPlayer(player: Player) {
+    player.currentState= "InGameLobby";
+    this._players.push(player);
+    this.sendMatchIdToPlayer(player);
   }
 
   private isBeforFinalPlayer(): boolean {
@@ -53,6 +73,37 @@ class Match {
 
   private isFinalPlayer(): boolean {
     return (this._players.length === this._numberOfPlayers - 1);
+  }
+
+  async startGame(): Promise<void> {
+    //await this.addMatchToDB();
+    //await this.addPlayersToMatchInDB();
+    this._gameStatus = "playing";
+    // start louens game
+  }
+
+  async addMatchToDB(): Promise<void> { // make private
+    if (!this._connectionToDB)
+      return;
+    const sql = `
+    INSERT INTO matches (id, game, gameMode)
+    VALUES (?, ?, ?)
+    `;
+    await this._connectionToDB.run(sql, [
+      this._matchId, "Pong", this._gameOptions.gameType]);
+  }
+
+  async addPlayersToMatchInDB(): Promise<void> { // make private
+    if (!this._connectionToDB)
+      return;
+    const sql = `
+    INSERT INTO r_users_matches (id, userId, match)
+    VALUES (?, ?, ?)
+    `;
+    this._players.forEach(async player => {
+      await this._connectionToDB.run(sql,
+                      [randomUUID, player.playerId, this._matchId]);
+    });
   }
 
   sendMessageToAllPlayers(message: string) {
@@ -73,14 +124,30 @@ class Match {
     return (this._players.length === this._numberOfPlayers);
   }
 
-  addMatchToDB(): void {
-    if (!this._connectionToDB)
-      return;
-    const sql = `INSERT INTO matches (id, game, gameMode) VALUES (?, ?, ?)`;
-    this._connectionToDB.run(sql, [this._matchId, "Pong", this._gameOptions.gameType]);
+  private getPlayerNameAndId(): string {
+  return this._players
+    .map(player => `PlayerId: ${player.playerId}, PlayerName: ${player.userName}`)
+    .join("\n");
   }
+
+  public printGameStats() {
+  const message = "MatchId: " + this._matchId + "\n"
+    + " GameOptions: " + JSON.stringify(this._gameOptions) + "\n"
+    + " Players:\n" + this.getPlayerNameAndId() + "\n"
+    + " NumberOfPlayers: " + this._numberOfPlayers + "\n"
+    + " GameStatus: " + this._gameStatus + "\n";
+  console.log(message);
+  }
+
 }
 
+type GameOptions =
+  { gameMode: 'pong'; gameType: 'VanillaDouble'; }
+  | { gameMode: 'pong'; gameType: 'ModdedDouble'; modifiers: Modifiers[] }
+  | { gameMode: 'pong'; gameType: 'VanillaMulti'; }
+  | { gameMode: 'pong'; gameType: 'ModdedMulti'; modifiers: Modifiers[] };
+
+        type Modifiers = "blackwhole" | "speedUpBall";
 
 
 export type { GameOptions, Modifiers };
