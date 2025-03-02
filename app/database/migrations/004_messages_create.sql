9b38ce527396bd5a68ab664c47fc5c995e9c0f1d
pgrossma <pgrossma@student.42heilbronn.de>
2025-03-02 16:49:01 +0000

    added more db tables fors chats

--- app/database/migrations/004_messages_create.sql
+++ app/database/migrations/004_messages_create.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS messages (
-	sender TEXT NOT NULL,
-	receiver TEXT NOT NULL,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	room_id INTEGER NOT NULL,
 	message TEXT NOT NULL,
 	timestamp TIMESTAMP NOT NULL,
-	Primary Key (sender, receiver, timestamp),
-	FOREIGN KEY (sender) REFERENCES users(id),
-	FOREIGN KEY (receiver) REFERENCES users(id)
+	FOREIGN KEY (room_id) REFERENCES chat_rooms(id)
 );
---
+++ app/database/migrations/010_r_users_chat.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users_chat (
+    user_id TEXT NOT NULL,
+    room_id TEXT NOT NULL,
+    PRIMARY KEY (user_id, room_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (room_id) REFERENCES chat_rooms(id)
+);
