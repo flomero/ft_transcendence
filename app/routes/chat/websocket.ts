136219e201f904ad94f33cf622dd89e0d6ddf4bd
Paul Großmann <git@paulgrossmann.de>
2025-03-02 23:07:45 +0000

    chat message sending to all clients works now

--- app/routes/chat/websocket.ts
+++ app/routes/chat/websocket.ts
@@ -5,29 +5,61 @@ interface ChatMessageResponse {
     html: string;
 }
 
+interface SocketClient {
+    socket: any;
+    roomId: number;
+}
+
+const clients: SocketClient[] = [];
+
 const chatWebSocket: FastifyPluginAsync = async (fastify): Promise<void> => {
     fastify.get('/ws', { websocket: true }, async function (socket, request) {
         fastify.log.trace('Chat client connected');
 
+        const roomId = 1;
+        clients.push({ socket: socket, roomId: roomId });
+
         socket.on('message', async function (message) {
             fastify.log.trace('Received message: ' + message);
 
-            await postMessage(fastify, 1, "110899881598177411832", message.toString());
+            for (const client of clients) {
+                if (client.roomId == 1) {
+                    const html = await fastify.view('partials/chat/message', {
+                        message: {
+                            userName: "John Doe",
+                            message: message,
+                            timestamp: new Date().toLocaleString(),
+                            isOwnMessage: true
+                        }
+                    })
 
-            const html = await fastify.view('partials/chat/message', {
-                message: {
-                    userName: "John Doe",
-                    message: message,
-                    timestamp: new Date().toLocaleString(),
-                    isOwnMessage: true
+                    const response: ChatMessageResponse = {
+                        html: html
+                    };
+
+                    client.socket.send(JSON.stringify(response));
                 }
-            })
+            }
 
-            const response: ChatMessageResponse = {
-                html: html
-            };
+            await postMessage(fastify, 1, "110899881598177411832", message.toString());
+        });
 
-            socket.send(JSON.stringify(response));
+        socket.on('close', async function () {
+            fastify.log.trace('Chat client disconnected');
+
+            const index = clients.findIndex(client => client.socket === socket);
+            if (index !== -1) {
+                clients.splice(index, 1);
+            }
+        });
+
+        socket.on('error', async function (error) {
+            fastify.log.trace('Chat client error', error);
+
+            const index = clients.findIndex(client => client.socket === socket);
+            if (index !== -1) {
+                clients.splice(index, 1);
+            }
         });
     });
 }
