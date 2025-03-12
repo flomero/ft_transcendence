665de77cd98eb1421bafdc1efbf8cc4692fd7b50
Fritz Gäbler <fritzlmgaebler@gmail.com>
2025-03-12 16:40:34 +0100

    add basic construct of start lobby
    Feature needs to be added so lobby can just be started if every user is
    ready.

--- app/routes/games/lobby/start/startLobby.ts
+++ app/routes/games/lobby/start/startLobby.ts
@@ -1,10 +1,8 @@
 import { FastifyPluginAsync } from "fastify";
-import startLobbySchema from "../../../../schemas/games/lobby/startLobbySchema";
 import startLobbyHandler from "../../../../services/games/lobby/start/startLobbyHandler";
 
 const startLobby: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.post("/", {
-    schema: startLobbySchema,
+  fastify.post("/:lobbyId", {
     handler: startLobbyHandler,
   });
 };
--- app/services/games/lobby/start/startLobbyHandler.ts
+++ app/services/games/lobby/start/startLobbyHandler.ts
@@ -1,7 +1,22 @@
 import { FastifyRequest, FastifyReply } from "fastify";
+import { validConnectionCheck } from "../lobbyVaidation/validConnectionCheck";
+import setLobbyStateToStart from "./setLobbyStateToStart";
 
-async function startLobbyHandler(request: FastifyRequest, reply: FastifyReply) {
-  reply.send({ message: "Lobby started" });
+async function startLobbyHandler(
+  request: FastifyRequest<{ Params: { lobbyId: string } }>,
+  reply: FastifyReply,
+) {
+  const lobbyId = request.params.lobbyId;
+  const userId = request.userId;
+
+  try {
+    validConnectionCheck(userId, lobbyId);
+    setLobbyStateToStart(lobbyId, userId);
+    reply.code(200).send({ message: "Lobby started" });
+  } catch (error) {
+    if (error instanceof Error)
+      reply.code(400).send({ message: error.message });
+  }
 }
 
 export default startLobbyHandler;
