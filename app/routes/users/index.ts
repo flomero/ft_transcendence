66fd4672866bf30df7080929f2895fb3e9b0fe49
flomero <49640251+flomero@users.noreply.github.com>
2025-03-25 15:24:46 +0000

    add user page and block buttons

--- app/routes/users/index.ts
+++ app/routes/users/index.ts
@@ -1,5 +1,7 @@
 import type { FastifyPluginAsync } from "fastify";
-import { getUserById, type UserWithImage } from "../../services/database/user";
+import { getUserById } from "../../services/database/user";
+import { isFriend } from "../../services/database/friend/friends";
+import { hasInvite } from "../../services/database/friend/invites";
 
 const profile: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.get("/:userId", async (request, reply) => {
@@ -12,11 +14,36 @@ const profile: FastifyPluginAsync = async (fastify): Promise<void> => {
       return reply.status(404).send({ message: "User not found" });
     }
 
-    const data: UserWithImage = {
+    const isFriendOfCurrentUser = await isFriend(
+      fastify,
+      request.userId,
+      userId,
+    );
+
+    const requestSent = await hasInvite(fastify, request.userId, userId);
+
+    const requestReceived = await hasInvite(fastify, userId, request.userId);
+
+    const showFriendButton =
+      !isFriendOfCurrentUser && !requestSent && !requestReceived;
+    const showRemoveButton = isFriendOfCurrentUser;
+    const showRequestButtons = !isFriendOfCurrentUser && requestReceived;
+
+    const data = {
       userId: userData.id,
       userName: userData.username,
       imageUrl: `/image/${userData.image_id}`,
+      isFriend: isFriendOfCurrentUser,
+      requestSent: requestSent,
+      showFriendButton: showFriendButton,
+      showRemoveButton: showRemoveButton,
+      showRequestButtons: showRequestButtons,
+      isBlocked: false, // TODO: add function
+      showText: true,
     };
+
+    console.table(data);
+
     const viewOptions = request.isAjax() ? {} : { layout: "layouts/main" };
     return reply.view("views/user", data, viewOptions);
   });
