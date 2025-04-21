32f0b692429b48ecdd780c0cafee12b9fcb18928
Paul Großmann <git@paulgrossmann.de>
2025-04-21 12:51:59 +0000

    new colum in messages for msg type

--- app/database/migrations/004_messages_create.sql
+++ app/database/migrations/004_messages_create.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS messages
     room_id   INTEGER   NOT NULL,
     sender_id TEXT      NOT NULL,
     message   TEXT      NOT NULL,
+    type      TEXT      NOT NULL DEFAULT 'USER',
     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (room_id) REFERENCES chat_rooms (id) ON DELETE CASCADE,
     FOREIGN KEY (sender_id) REFERENCES users (id) ON DELETE CASCADE
