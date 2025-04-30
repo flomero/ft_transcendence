a20e0e2db55b3f2c2768b07c08dcddf68d7502ce
Paul Großmann <git@paulgrossmann.de>
2025-04-30 19:27:49 +0000

    add invite endpoint for rommIds

--- app/routes/games/lobby/invite.ts
+++ app/routes/games/lobby/invite.ts
@@ -1,9 +1,13 @@
 import type { FastifyPluginAsync } from "fastify";
 import { isFriend } from "../../../services/database/friend/friends";
-import { sendGameInviteToUser } from "../../../services/chat/live";
+import {
+  sendGameInvite,
+  sendGameInviteToUser,
+} from "../../../services/chat/live";
+import { userIsInRoom } from "../../../services/database/chat/room";
 
 const inviteLobby: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.post("/:lobbyId/invite/:friendId", async (request, reply) => {
+  fastify.post("/:lobbyId/invite/friend/:friendId", async (request, reply) => {
     const { friendId, lobbyId } = request.params as {
       friendId: string;
       lobbyId: number;
@@ -15,6 +19,27 @@ const inviteLobby: FastifyPluginAsync = async (fastify): Promise<void> => {
 
     try {
       await sendGameInviteToUser(fastify, request, friendId, lobbyId);
+    } catch (error) {
+      if (error instanceof Error) return reply.badRequest(error.message);
+      return reply.badRequest("Error sending invite");
+    }
+
+    return reply.code(200).send({ message: "Successfully send invite" });
+  });
+
+  fastify.post("/:lobbyId/invite/room/:roomId", async (request, reply) => {
+    const { lobbyId, roomId } = request.params as {
+      roomId: number;
+      lobbyId: number;
+    };
+    if (!lobbyId || !roomId) return reply.badRequest("No lobbyId or roomId");
+
+    if (!(await userIsInRoom(fastify, roomId, request.userId))) {
+      return reply.badRequest("Room does not exist or you are not in it");
+    }
+
+    try {
+      await sendGameInvite(fastify, request, roomId, lobbyId);
     } catch (error) {
       if (error instanceof Error) return reply.badRequest(error.message);
       return reply.badRequest("Error sending invite");
