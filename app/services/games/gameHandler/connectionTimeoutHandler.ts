46ffc9cac21861cba2719237d633ebaa042388bc
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-05-03 12:42:10 +0200

    add new logic to disquallify user if not connected after 50 sec

--- app/services/games/gameHandler/connectionTimeoutHandler.ts
+++ app/services/games/gameHandler/connectionTimeoutHandler.ts
@@ -1,20 +1,29 @@
 import GameManager from "../gameHandler/GameManager";
-import { GameStatus } from "../gameBase";
+import { GameStatus } from "../../../types/games/gameBaseState";
 import type { Database } from "sqlite";
 
 const connectionTimeoutHandler = (gameManager: GameManager, db: Database) => {
-  setTimeout(() => disconnectMembersIfNotConnected(gameManager, db), 25000);
+  setTimeout(() => startGameIfNotAllPlayerConnected(gameManager, db), 15000);
+  setTimeout(
+    () => disqualifyNotConnectedPlayersIfOnePlayer(gameManager),
+    50000,
+  );
 };
 
-const disconnectMembersIfNotConnected = (
+const startGameIfNotAllPlayerConnected = (
   gameManager: GameManager,
   db: Database,
 ) => {
   if (gameManager.allPlayersAreConnected() === true) return;
 
-  if (gameManager.gameStatus === GameStatus.CREATED) {
+  if (gameManager.gameStatus() === GameStatus.CREATED) {
     gameManager.startGame(db);
   }
+};
+
+const disqualifyNotConnectedPlayersIfOnePlayer = (gameManager: GameManager) => {
+  if (gameManager.connectedNumberOfPlayersInGame() === 1)
+    gameManager.disqualifyNotConnectedPlayers();
 };
 
 export default connectionTimeoutHandler;
