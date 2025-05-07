d8366814925862efed05864d30d196addd9fadbc
Fritz Gäbler <58051701+fritz-gabler@users.noreply.github.com>
2025-05-07 16:33:53 +0200

    alter game score to db

--- app/database/migrations/007_r_users_matches.sql
+++ app/database/migrations/007_r_users_matches.sql
@@ -2,8 +2,6 @@ CREATE TABLE IF NOT EXISTS r_users_matches
 (
     userId  TEXT    NOT NULL,
     matchId TEXT    NOT NULL,
-    score   INTEGER NOT NULL,
-    result TEXT,
     PRIMARY KEY (userId, matchId),
     FOREIGN KEY (userId) REFERENCES users (id),
     FOREIGN KEY (matchId) REFERENCES matches (id)
