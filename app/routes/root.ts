576ff9887ea407f751133bf7e963f6c361415436
Johannes Moritz <johannes.moritz@stackit.cloud>
2025-05-11 21:27:58 +0200

    feat: update navigation and redirect home route to /play

--- app/routes/root.ts
+++ app/routes/root.ts
@@ -2,17 +2,18 @@ import type { FastifyPluginAsync } from "fastify";
 
 const root: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.get("/", async (request, reply) => {
-    const viewOptions = request.isAjax() ? {} : { layout: "layouts/main" };
-    return reply.view(
-      "views/home",
-      {
-        title: "ft_transcendence",
-        isAuthenticated: request.isAuthenticated,
-        userId: request.userId,
-        userName: request.userName,
-      },
-      viewOptions,
-    );
+    return reply.redirect("/play");
+    // const viewOptions = request.isAjax() ? {} : { layout: "layouts/main" };
+    // return reply.view(
+    //   "views/home",
+    //   {
+    //     title: "ft_transcendence",
+    //     isAuthenticated: request.isAuthenticated,
+    //     userId: request.userId,
+    //     userName: request.userName,
+    //   },
+    //   viewOptions,
+    // );
   });
 
   fastify.get("/health", async () => ({ status: "ok" }));
