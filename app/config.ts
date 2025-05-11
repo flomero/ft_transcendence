ed0ab729f70173a20218d0229d6b65b074838dbe
Florian Fischer <49640251+flomero@users.noreply.github.com>
2025-05-11 20:31:22 +0200

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -52,6 +52,7 @@ export const TournamentGameModesPerBracketType: {
   singleElimination: [
     TournamentGameModes.CompetitiveClassicPong,
     TournamentGameModes.CompetitiveClassicPongPowerUps,
+    TournamentGameModes.PowerUpMayhem1v1,
   ],
   doubleElimination: [
     TournamentGameModes.CompetitiveClassicPong,
